b25aaccc4fae6a47eaa142a750ba6b93e3397416
Chip Collier <chip@kopernikusauto.com>
2020-08-03 15:20:28 +0200

    Enable building the core library with a static msvc runtime.. and as a static library

--- cmake/helper_functions/ecal_add_functions.cmake
+++ cmake/helper_functions/ecal_add_functions.cmake
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,8 +21,8 @@ function(ecal_add_app_console TARGET_NAME)
   add_executable(${TARGET_NAME} ${ARGN})
   ecal_set_subsystem_console(${TARGET_NAME})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME})
 endfunction()
 
@@ -31,15 +31,15 @@ endfunction()
 function(ecal_add_gtest TARGET_NAME)
   add_executable(${TARGET_NAME} ${ARGN})
   add_test(
-    NAME              ${TARGET_NAME} 
+    NAME              ${TARGET_NAME}
     COMMAND           $<TARGET_FILE:${TARGET_NAME}>
     WORKING_DIRECTORY $<TARGET_FILE_DIR:${TARGET_NAME}>
   )
   target_link_libraries(${TARGET_NAME} PRIVATE GTest::GTest GTest::Main)
   #ecal_set_subsystem_windows(${TARGET_NAME})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME})
 endfunction()
 
@@ -47,16 +47,16 @@ function(ecal_add_app_gui TARGET_NAME)
   add_executable(${TARGET_NAME} ${ARGN})
   ecal_set_subsystem_windows(${TARGET_NAME})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME})
 endfunction()
 
 function(ecal_add_app_qt TARGET_NAME)
   add_executable(${TARGET_NAME} ${ARGN})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME})
   if(WIN32)
     set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS "/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup")
@@ -67,7 +67,7 @@ function(ecal_add_mon_plugin TARGET_NAME)
   set(options        "")
   set(oneValueArgs   METADATA)
   set(multiValueArgs SOURCES)
-  cmake_parse_arguments(MON_PLUGIN 
+  cmake_parse_arguments(MON_PLUGIN
    "${options}"
    "${oneValueArgs}"
    "${multiValueArgs}"
@@ -75,11 +75,11 @@ function(ecal_add_mon_plugin TARGET_NAME)
   )
   add_library(${TARGET_NAME} MODULE ${MON_PLUGIN_SOURCES} ${MON_PLUGIN_METADATA})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION $<NOT:$<CXX_COMPILER_ID:Clang,AppleClang>:${${TARGET_NAME}_VERSION}>>
-    SOVERSION $<NOT:$<CXX_COMPILER_ID:Clang,AppleClang>:${${TARGET_NAME}_VERSION_MAJOR}>>
+    # VERSION $<NOT:$<CXX_COMPILER_ID:Clang,AppleClang>:${${TARGET_NAME}_VERSION}>>
+    # SOVERSION $<NOT:$<CXX_COMPILER_ID:Clang,AppleClang>:${${TARGET_NAME}_VERSION_MAJOR}>>
     LIBRARY_OUTPUT_DIRECTORY $<IF:$<BOOL:${WIN32}>,${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>/ecalmon_plugins,${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/ecal/plugins/mon>
   )
-    
+
 endfunction()
 
 function(ecal_add_rec_addon TARGET_NAME)
@@ -90,23 +90,23 @@ function(ecal_add_rec_addon TARGET_NAME)
     OUTPUT_NAME ecal_${TARGET_NAME}
     RUNTIME_OUTPUT_DIRECTORY $<IF:$<BOOL:${WIN32}>,${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>/ecalrec_addons,${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/ecal/addons/rec>
   )
-    
+
 endfunction()
 
 function(ecal_add_time_plugin TARGET_NAME)
   add_library(${TARGET_NAME} MODULE ${ARGN})
-  set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
-  )
+  # set_target_properties(${TARGET_NAME} PROPERTIES
+  #   VERSION ${eCAL_VERSION_STRING}
+  #   SOVERSION ${eCAL_VERSION_MAJOR}
+  # )
 endfunction()
 
 # this appends the 64 / 32 suffix (required for the eCAL Core libraries)
 function(ecal_add_ecal_shared_library TARGET_NAME)
   add_library(${TARGET_NAME} SHARED ${ARGN})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME})
 endfunction()
 
@@ -114,8 +114,8 @@ endfunction()
 function(ecal_add_ecal_static_library TARGET_NAME)
   add_library(${TARGET_NAME} STATIC ${ARGN})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME}
     POSITION_INDEPENDENT_CODE ON)
 endfunction()
@@ -132,17 +132,17 @@ endfunction()
 function(ecal_add_shared_library TARGET_NAME)
   add_library(${TARGET_NAME} SHARED ${ARGN})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME})
 endfunction()
 
 function(ecal_add_static_library TARGET_NAME)
   add_library(${TARGET_NAME} STATIC ${ARGN})
   set_property(TARGET ${TARGET_NAME} PROPERTY POSITION_INDEPENDENT_CODE ON)
-  set_target_properties(${TARGET_NAME} PROPERTIES 
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+  set_target_properties(${TARGET_NAME} PROPERTIES
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_${TARGET_NAME})
 endfunction()
 
@@ -161,8 +161,8 @@ endfunction()
 function(ecal_add_sample TARGET_NAME)
   add_executable(${TARGET_NAME} ${ARGN})
   set_target_properties(${TARGET_NAME} PROPERTIES
-    VERSION ${eCAL_VERSION_STRING}
-    SOVERSION ${eCAL_VERSION_MAJOR}
+    # VERSION ${eCAL_VERSION_STRING}
+    # SOVERSION ${eCAL_VERSION_MAJOR}
     OUTPUT_NAME ecal_sample_${TARGET_NAME})
 endfunction()
 
--- ecal/core/include/ecal/ecal_os.h
+++ ecal/core/include/ecal/ecal_os.h
@@ -5,9 +5,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,12 +15,12 @@
  * limitations under the License.
  *
  * ========================= eCAL LICENSE =================================
-*/
+ */
 
 /**
  * @file   ecal_os.h
  * @brief  eCAL os interface
-**/
+ **/
 
 #pragma once
 
@@ -37,8 +37,7 @@
 #define ECAL_OS_MACOS
 #endif
 
-
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && !defined(ECAL_STATIC)
   #ifdef eCAL_EXPORTS
     #define ECALC_API __declspec(dllexport)
   #else /* eCAL_EXPORTS */
